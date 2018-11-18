d75f320ae8f026ae00c1701fe2fcaa9410df139b
bianyandong <bydcyh147@qq.com>
2018-11-18 17:29:13 +0800

    commit

---
+++ js/hero.js
@@ -0,0 +1,184 @@
+let menu=[
+	{
+		country:"蜀国",
+		img:["./img/sgs/shuguo/liubei.png","./img/sgs/shuguo/guanyu.png",
+		"./img/sgs/shuguo/zhangfei.png","./img/sgs/shuguo/zhugeliang.png","./img/sgs/shuguo/zhaoyun.png","./img/sgs/shuguo/machao.png"
+		],
+		skin:[
+			[
+					"./img/sgs/shuguo/liubei2.png","./img/sgs/shuguo/liubei3.png",
+				"./img/sgs/shuguo/liubei4.png","./img/sgs/shuguo/liubei5.png"],
+				["./img/sgs/shuguo/guanyu2.png","./img/sgs/shuguo/guanyu3.png","./img/sgs/shuguo/guanyu4.png"],
+				["./img/sgs/shuguo/zhangfei2.png","./img/sgs/shuguo/zhangfei3.png"],
+				["./img/sgs/shuguo/zhugeliang2.png","./img/sgs/shuguo/zhugeliang3.png","./img/sgs/shuguo/zhugeliang4.png"],
+				["./img/sgs/shuguo/zhaoyun2.png","./img/sgs/shuguo/zhaoyun3.png","./img/sgs/shuguo/zhaoyun4.png"],
+				["./img/sgs/shuguo/machao2.png","./img/sgs/shuguo/machao3.png","./img/sgs/shuguo/machao4.png"]
+			
+		],
+		life:["先主姓刘，讳备，字玄德，涿郡涿县人，汉景帝子中山靖王胜之后也。以仁德治天下。",
+		"字云长，本字长生，并州河东解州人。五虎上将之首，爵至汉寿亭侯，谥曰“壮缪侯”。被奉为“关圣帝君”，崇为“武圣”。",
+		"字翼德，涿郡人，燕颔虎须，豹头环眼。有诗云：“长坂坡头杀气生，横枪立马眼圆睁。一声好似轰雷震，独退曹家百万兵”。",
+		"字孔明，号卧龙，琅琊阳都人，蜀汉丞相。在世时被封为武乡侯，谥曰忠武侯。著有《出师表》、《诫子书》等。怀不世之才，以空城戏司马，能观星象而通鬼神。",
+		"字子龙，常山真定人。身长八尺，姿颜雄伟。长坂坡单骑救阿斗，先主云：“子龙一身都是胆也。”","字孟起，扶风茂陵人。面如冠玉，目如流星，虎体猿臂，彪腹狼腰，声雄力猛。因衣着讲究，举止非凡，故人称“锦马超”。麾铁骑，捻金枪。",
+		],
+		trait:["能够将手牌分配给队友。没有手牌时很脆弱。","拥有大量的【杀】，能够每回合稳定使用【杀】。需要武器牌提供攻击范围。",
+		"能够迅速杀死脆弱的武将。需要武器牌提供攻击范围，且【杀】越多越好。","能够控制牌堆顶的牌序，决定自己和下家的判定牌和摸牌。尽量把牌用光。",
+		"进可攻退可守。受到【贯石斧】等强制造成伤害的牌的针对。","能够让【杀】强制造成伤害，拥有更大的攻击范围。需要有较多的【杀】，受到【乐不思蜀】的限制。"],
+		skill:["仁德","武胜","咆哮","观星","龙胆","马术"],
+		skill2:["激将","单骑","替身","空城","涯角","铁骑"],
+		skillInfo:["仁德 出牌阶段，你可以将任意张手牌交给其他角色，然后你于此阶段内给出第二张手牌时，你回复1点体力。","武圣 你可以将一张红色牌当【杀】使用或打出。",
+		"咆哮 锁定技，你使用【杀】无次数限制。","观星 准备阶段，你可以观看牌堆顶的X张牌（X为存活角色数且最多为5），然后以任意顺序放回牌堆顶或牌堆底。",
+		"龙胆 你可以将一张【杀】当【闪】、【闪】当【杀】使用或打出。","马术 锁定技，你计算与其他角色的距离-1。"],
+		skillInfo2:["激将 主公技，其他蜀势力角色可以在你需要时代替你使用或打出【杀】。","单骑 觉醒技，准备阶段，若你的手牌数大于体力值且本局游戏的主公不是刘备，你减1点体力上限，然后获得'马术'和'怒斩'。",
+		"替身 限定技，准备阶段，你可以将体力回复至等同于你上回合结束后的体力，然后摸X张牌(X为你回复的体力值)。","空城 锁定技，若你没有手牌，你不能成为【杀】或【决斗】的目标。","涯角 当你于回合外使用或打出手牌时，你可以展示牌堆顶的一张牌。若这两张牌的类别相同，你可以将展示的牌交给一名角色；若类别不同，你可以将展示的牌置入弃牌堆。"
+		,"铁骑 当你使用【杀】指定目标后，你可以进行判定，若结果为红色，该角色不能使用【闪】。"
+		],
+		sound:["./img/sgs/shuguo/仁德.mp3","./img/sgs/shuguo/武胜.mp3","./img/sgs/shuguo/咆哮.mp3","./img/sgs/shuguo/观星.mp3","./img/sgs/shuguo/龙胆.mp3",
+		"./img/sgs/shuguo/马术.mp3"],
+		sound2:["./img/sgs/shuguo/激将.mp3","./img/sgs/shuguo/单骑.mp3","./img/sgs/shuguo/替身.mp3","./img/sgs/shuguo/空城.mp3","./img/sgs/shuguo/涯角.mp3",
+		"./img/sgs/shuguo/铁骑.mp3"]
+},
+	{
+		country:"魏国",
+		img:["./img/sgs/weiguo/caocao.png","./img/sgs/weiguo/simayi.png","./img/sgs/weiguo/caopi.png"],
+		skin:[
+		["./img/sgs/weiguo/caocao2.png","./img/sgs/weiguo/caocao3.png"],
+		["./img/sgs/weiguo/simayi2.png","./img/sgs/weiguo/simayi3.png","./img/sgs/weiguo/simayi4.png"],
+		["./img/sgs/weiguo/caopi2.png","./img/sgs/weiguo/caopi3.png"]
+		],
+		life:["魏武帝曹操，字孟德，小名阿瞒、吉利，沛国谯人。精兵法，善诗歌，乃治世之能臣，乱世之奸雄也。","晋宣帝，字仲达，河内温人。曾任职过曹魏的大都督，太尉，太傅。少有奇节，聪明多大略，博学洽闻，伏膺儒教，世之鬼才也。",
+		"字子桓，三国时期著名的政治家、文学家，曹魏的开国皇帝，公元220－226年在位。沛国谯人，魏武帝曹操与武宣卞皇后的长子。去世后庙号高祖，谥为文皇帝，葬于首阳陵。"],
+		trait:["能够获得敌人的攻击牌用来攻击敌人，利用【南蛮入侵】和【万箭齐发】攻击全场。受到【乐不思蜀】的限制。","控制其他角色的判定牌，利用【闪电】杀死脆弱的武将。受到【寒冰剑】的针对，没有手牌时很脆弱。",
+		"受伤时让敌人跳过行动，拿走死亡角色的牌。没有牌时很脆弱."],
+		skill:["奸雄","反馈","行殇"],
+		skill2:["护驾","鬼才","放逐"],
+		skillInfo:["奸雄 当你受到伤害后，你可以获得造成此伤害的牌。",
+		 "反馈 当你受到伤害后，你可以获得伤害来源的一张牌。","行殇 当其他角色死亡时，你可以获得其所有牌。"],
+		skillInfo2:["护驾 主公技，其他魏势力角色可以在你需要时代替你使用或打出【闪】。","鬼才 当一名角色的判定牌生效前，你可以打出一张手牌代替之。","放逐 当你受到伤害后，你可以令一名其他角色翻面，然后该角色摸X张牌（X为你已损失的体力值）。"],
+		sound:["./img/sgs/weiguo/奸雄.mp3","./img/sgs/weiguo/反馈.mp3","./img/sgs/weiguo/行殇.mp3"],
+		sound2:["./img/sgs/weiguo/护驾.mp3","./img/sgs/weiguo/鬼才.mp3","./img/sgs/weiguo/放逐.mp3"]
+	},
+	{
+		country:"吴国",
+		img:["./img/sgs/wuguo/sunquan.png","./img/sgs/wuguo/taishici.png",
+		"./img/sgs/wuguo/ganning.png","./img/sgs/wuguo/luxun.png"],
+		skin:[
+		["./img/sgs/wuguo/sunquan2.png","./img/sgs/wuguo/sunquan3.png"],
+		["./img/sgs/wuguo/taishici2.png"],
+		["./img/sgs/wuguo/ganning2.png","./img/sgs/wuguo/ganning3.png"],
+		["./img/sgs/wuguo/luxun2.png","./img/sgs/wuguo/luxun3.png","./img/sgs/wuguo/luxun4.png"]
+		],
+		life:["吴大帝，字仲谋，吴郡富春县人。统领吴与蜀魏三足鼎立，制衡天下。","太史慈，字子义，东莱黄县（今山东龙口东黄城集）人。东汉末年武将，守言应诺，恪遵信义。官至建昌都尉。弓马熟练，箭法精良。",
+		"字兴霸，巴郡临江人，祖籍荆州南阳郡。为人勇猛刚强，忠心耿耿，勇往无前。曾带兵百人于二更奇袭曹营，大挫其锐气。","本名陆议，字伯言，吴郡吴县人。历任东吴大都督、丞相。吴大帝孙权兄孙策之婿，世代为江东大族。以谦逊之书麻痹关羽，夺取荆州，又有火烧连营大破蜀军。"],
+		trait:["能够弃掉无用的牌并摸新的牌。个人能力突出，配合较弱。","能够使用无距离限制的【杀】攻击两个敌人。【杀】和大点数的手牌越多越好，受到【乐不思蜀】的限制。","能够转化大量的【过河拆桥】，弃置敌人的牌使其变得脆弱。没有手牌时很脆弱。","能够源源不断的使用手牌，回合内拥有高爆发，回合外拥有高防御。受到【贯石斧】等强制造成伤害的牌的针对。"],
+		skill:["制衡","天义","奇袭","谦逊"],
+		skill2:["救援","不屈","奋威","连营"],
+		skillInfo:["制衡 出牌阶段限一次，你可以弃置任意张牌，然后摸等量的牌。","天义 出牌阶段限一次，你可以与一名角色拼点：若你赢，本回合你可以多使用一张【杀】、使用【杀】无距离限制且可以多选择一个目标；若你没赢，本回合你不能使用【杀】。","奇袭 你可以将一张黑色牌当【过河拆桥】使用。",
+		"谦逊 锁定技，你不能成为【顺手牵羊】和【乐不思蜀】的目标。"],
+		skillInfo2:["救援 主公技，锁定技，其他吴势力角色对你使用【桃】回复的体力+1。","不屈 锁定技，当你处于濒死状态时，你将牌堆顶的一张牌置于你的武将牌上，称为'创'。",
+		"奋威 限定技，当一张锦囊牌指定多个目标后，你可令此牌对其中任意个目标无效。","连营 当你失去最后的手牌时，你可以摸一张牌。"],
+		sound:["./img/sgs/wuguo/制衡.mp3","./img/sgs/wuguo/天义.mp3","./img/sgs/wuguo/奇袭.mp3","./img/sgs/wuguo/谦逊.mp3"],
+		sound2:["./img/sgs/wuguo/救援.mp3","./img/sgs/wuguo/不屈.mp3","./img/sgs/wuguo/奋威.mp3","./img/sgs/wuguo/连营.mp3"]
+	},
+	{
+		country:"群雄",
+		img:["./img/sgs/qun/zhangjiao.png","./img/sgs/qun/diaochan.png","./img/sgs/qun/gaoshun.png"],
+		skin:[
+			["./img/sgs/qun/zhangjiao2.png"],
+			["./img/sgs/qun/diaochan2.png","./img/sgs/qun/diaochan3.png"],
+			["./img/sgs/qun/gaoshun2.png","./img/sgs/qun/gaoshun3.png","./img/sgs/qun/gaoshun4.png"]
+		],
+		life:["乱世的开始，黄巾起义军首领，太平道创始人。张角早年信奉黄老学说，对在汉代十分流行的谶纬之学也深有研究，对民间医术 、巫术也很熟悉。","中国古代四大美女之一，有闭月羞花之貌。司徒王允之义女，由王允授意施行连环计，离间董卓、吕布，借布手除卓。后貂蝉成为吕布的妾。",
+		"中国东汉末年将领，吕布帐下中郎将。史载高顺为人清白有威严，不好饮酒，所统率的部队精锐非常，号称“陷阵营”。屡进忠言于吕布，吕布虽知其忠而不能用。曹操击破吕布后，高顺被曹操所杀。"],
+		trait:["控制其他角色的判定牌，依靠【闪】和【闪电】杀死脆弱的武将。没有【闪】时很脆弱。","使男性敌人互相决斗，能获得大量手牌。会成为敌人优先攻击的目标。","能够使用较多的【杀】，能够迅速杀死脆弱的武将。不能使用【酒】。"],
+		skill:["雷击","离间","陷阵"],
+		skill2:["鬼道","闭月","禁酒"],
+		skillInfo:["当你使用或打出【闪】时，令一名其他角色进行判定，若为：黑桃，对其造成2点雷电伤害；梅花，你回复1点体力，对其造成1点雷电伤害。","离间 出牌阶段限一次，你可以弃置一张牌，令一名男性角色视为对另一名男性角色使用一张【决斗】。","陷阵 出牌阶段限一次，你可以与一名角色拼点：若你赢，本回合你无视该角色的防具，且对其使用牌没有距离和次数限制；若你没赢，本回合你不能使用【杀】。"],
+		skillInfo2:["鬼道 当一名角色的判定牌生效前，你可以打出一张黑色牌替换之。","闭月 结束阶段，你可以摸一张牌。","禁酒 锁定技，你的【酒】视为【杀】。"],
+		sound:["./img/sgs/qun/雷击.mp3","./img/sgs/qun/离间.mp3","./img/sgs/qun/陷阵.mp3"],
+		sound2:["./img/sgs/qun/鬼道.mp3","./img/sgs/qun/闭月.mp3","./img/sgs/qun/禁酒.mp3"]
+	}
+	
+];
+
+let infoShow=false;
+let oldSound=document.getElementById("myPlay");
+	new Vue({
+		el:"#heroBox",
+		data:{
+			menu,
+			count:0, //菜单鼠标滑入
+			boxHidden:true,//英雄界面box的隐藏
+			infoShow,
+			newImg:'',
+			newLife:'',
+			newtrait:'',
+			skill:'',
+			skill2:'',
+			skillInfo:'',
+			skillInfo2:'',
+			sound:'',
+			sound2:'',
+			aaa:'',
+			bbb:'',
+			skillInfoFlag:true,
+			skillInfo2Flag:false,
+			otherSkin:{}
+
+		},
+		methods:{
+			show(count){ //判断鼠标移入哪个菜单
+				this.count=count;
+			},
+			showHero(heros,life,trait,skill,skill2,skillInfo,skillInfo2,sound,sound2,skin){ //武将详情界面显示
+				this.infoShow=true;
+				this.newImg=heros;
+				this.newLife=life;
+				this.newtrait=trait;
+				this.skill=skill;
+				this.skill2=skill2;
+				this.skillInfo=skillInfo;
+				this.skillInfo2=skillInfo2;
+				this.sound=sound;
+				this.sound2=sound2;
+				this.aaa=this.skillInfo;
+				this.bbb=this.skillInfo2;
+				//console.log(skin);
+				this.otherSkin=skin;
+				//console.log("我拿到的是"+this.otherSkin);
+				
+			},
+			close(){	//武将详情界面隐藏
+				this.infoShow=false;
+			},
+			playSound(){
+				oldSound.src=this.sound;
+				this.skillInfoFlag=true;
+				this.skillInfo2Flag=false;
+				oldSound.play();
+			
+			},
+			playSound2(){
+				oldSound.src=this.sound2;
+				this.skillInfo2Flag=true;
+				this.skillInfoFlag=false;
+				oldSound.play();
+			},
+			changeSkin(skins){
+				this.newImg=skins;
+			}
+		}		
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/sgs.js
@@ -0,0 +1,24 @@
+let country=[
+		{
+			country:"蜀国",
+			img:"./img/sgs/shu.jpg"
+		},
+		{
+			country:"魏国",
+			img:"./img/sgs/wei.jpg"
+		},
+		{
+			country:"吴国",
+			img:"./img/sgs/wu.jpg"
+		},
+		{
+			country:"群雄",
+			img:"./img/sgs/qun.jpg"
+		}
+	]
+	new Vue({
+		el:"#box",
+		data:{
+			country
+		}   
+	});
